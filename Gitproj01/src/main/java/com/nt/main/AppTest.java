30b073f1902d520aecfb6eb15313740a96a0f81e
HP <HP@DESKTOP-1E5BSEV>
2024-02-27 22:03:20 +0530

    task1 from dev1

--- Gitproj01/src/main/java/com/nt/main/AppTest.java
+++ Gitproj01/src/main/java/com/nt/main/AppTest.java
@@ -5,6 +5,11 @@ public class AppTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Arithmatic  ar =new Arithmatic();
+		 System.out.println("sum:"+ar.sum(10, 20));
+		 System.out.println("sub:"+ar.sub(200, 20));
+		
+		
 	}
 
 }
---
+++ Gitproj01/src/main/java/com/nt/main/Arithmatic.java
@@ -0,0 +1,14 @@
+package com.nt.main;
+
+public class Arithmatic {
+	
+	 public int sum(int x,int y)
+	 {
+		 return x+y;
+	 }
+	 public int sub(int x,int y)
+	 {
+		 return x-y;
+	 }
+
+}
