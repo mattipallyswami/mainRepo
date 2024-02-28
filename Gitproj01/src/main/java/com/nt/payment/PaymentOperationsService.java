9a99f339c779cdd4f9b7dffc35ffbff74294b52a
HP <HP@DESKTOP-1E5BSEV>
2024-02-28 21:46:35 +0530

    DEV1-Branch1-Task1

--- Gitproj01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ Gitproj01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -2,4 +2,19 @@ package com.nt.payment;
 
 public class PaymentOperationsService {
 
+	
+	public String getCreditCardOperations()
+	{
+		return "creditCrad Opertaion Suscess";
+	}
+	
+	
+	public String getDeditCardOperations()
+	{
+		return "deditCrad Opertaion Suscess";
+	}
+
+	
+	
+	
 }
